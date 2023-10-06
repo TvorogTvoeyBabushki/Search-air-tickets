175503add255f3f160e56609ad172b2844afecdf
foxta1ll <snake3773@mail.ru>
2023-10-06 16:04:53 +0300

    refactor: сделал декомпозицию компонента searchairtickets

--- src/components/screens/home/search-air-tickets/SearchAirTickets.tsx
+++ src/components/screens/home/search-air-tickets/SearchAirTickets.tsx
@@ -1,15 +1,12 @@
-import { FunctionComponent, useState, useEffect, useCallback } from 'react'
+import { FunctionComponent } from 'react'
 
 import { ITicketsData } from '@/api/data/tickets.data'
 
 import Button from '@/components/ui/button/Button'
-import Checkbox from '@/components/ui/checkbox/Checkbox'
-import RadioButton from '@/components/ui/radioButton/RadioButton'
 
-import { currency } from '@/utils/currency'
-
-import { useAction } from '@/hooks/useAction'
-
+import SearchAirTicketsWrapper from './tickets-wrapper/SearchAirTicketsWrapper'
+import SearchAirTicketsSortPanel from './sort-panel/SearchAirTicketsSortPanel'
+import { useSearchAirTickets } from './useSearchAirTickets'
 import styles from './SearchAirTickets.module.scss'
 
 const btnElements = [
@@ -18,182 +15,53 @@ const btnElements = [
 	{ children: 'Самый оптимальный', variant: 'optimal' }
 ]
 
+const searchAirTicketsSortItems = [
+	{
+		title: 'Количество пересадок',
+		variant: 'checkbox'
+	},
+	{
+		title: 'Компании',
+		variant: 'radiobtn'
+	}
+]
+
 const SearchAirTickets: FunctionComponent<{ [x: string]: ITicketsData[] }> = ({
 	tickets,
 	sortTicketsCompany,
 	sortTicketsConnectionAmount
 }) => {
-	const [ticketsData, setTicketsData] = useState<ITicketsData[]>([])
-	const {
-		SORT_CONNECTION_AMOUNT,
-		SORT_TICKETS_CHEAP,
-		SORT_TICKETS_FAST,
-		SORT_TICKETS_OPTIMAL,
-		SORT_COMPANY
-	} = useAction()
-
-	const [activeBtn, setActiveBtn] = useState<string[]>(['cheap'])
-	const [activeCheckbox, setActiveCheckbox] = useState<Set<string>>(new Set())
-	const [activeRadioBtn, setActiveRadioBtn] = useState<string[]>([
-		'все компании'
-	])
-
-	const [connectionAmount, setConnectionAmount] = useState<number[]>([])
-	const [companies, setCompanies] = useState<string[]>(['Все компании'])
-
-	const handleSortClick = (variant: string) => {
-		if (variant === 'cheap') {
-			SORT_TICKETS_CHEAP({
-				connectionAmount: activeCheckbox,
-				company: activeRadioBtn
-			})
-		}
-
-		if (variant === 'fast') {
-			SORT_TICKETS_FAST({
-				connectionAmount: activeCheckbox,
-				company: activeRadioBtn
-			})
-		}
-
-		if (variant === 'optimal') {
-			SORT_TICKETS_OPTIMAL({
-				connectionAmount: activeCheckbox,
-				company: activeRadioBtn
-			})
-		}
-		setActiveBtn([variant])
-	}
-
-	const handleCheckboxClick = useCallback(
-		(e: React.FormEvent<HTMLInputElement>) => {
-			setActiveCheckbox(prev => {
-				let filters = new Set(prev)
-
-				if ((e.target as HTMLInputElement).checked) {
-					filters.add((e.target as HTMLInputElement).value)
-				} else {
-					filters.delete((e.target as HTMLInputElement).value)
-				}
-
-				return filters
-			})
-		},
-		[setActiveCheckbox]
+	const searchAirTicketsProps = useSearchAirTickets(
+		tickets,
+		sortTicketsCompany,
+		sortTicketsConnectionAmount
 	)
-
-	const handleRadioBtnClick = (company: string) => {
-		setActiveRadioBtn([company])
-
-		if (company === 'все компании') {
-			activeCheckbox.size
-				? setTicketsData(sortTicketsConnectionAmount)
-				: setTicketsData(tickets)
-			return
-		}
-
-		SORT_COMPANY({
-			connectionAmount: activeCheckbox,
-			company: [company]
-		})
-	}
-
-	useEffect(() => {
-		if (activeRadioBtn[0] === 'все компании') {
-			handleSortClick(activeBtn[0])
-		}
-	}, [activeRadioBtn])
-
-	useEffect(() => {
-		if (activeCheckbox.size) {
-			SORT_CONNECTION_AMOUNT({
-				connectionAmount: activeCheckbox,
-				company: activeRadioBtn
-			})
-		} else {
-			activeRadioBtn[0] !== 'все компании'
-				? SORT_COMPANY({
-						connectionAmount: activeCheckbox,
-						company: activeRadioBtn
-				  })
-				: setTicketsData(tickets)
-		}
-		handleSortClick(activeBtn[0])
-	}, [activeCheckbox])
-
-	useEffect(() => {
-		if (tickets.length) {
-			setTicketsData(tickets)
-
-			tickets.forEach(ticket => {
-				setCompanies(prev => {
-					let companies = new Set(prev)
-					companies.add(ticket.company)
-
-					return [...companies]
-				})
-				setConnectionAmount(prev => {
-					let connectionAmount = new Set(prev)
-					connectionAmount.add(ticket.connectionAmount)
-
-					return [...connectionAmount]
-				})
-			})
-		}
-	}, [tickets])
-
-	useEffect(() => {
-		setTicketsData(sortTicketsCompany)
-	}, [sortTicketsCompany])
-
-	useEffect(() => {
-		setTicketsData(sortTicketsConnectionAmount)
-	}, [sortTicketsConnectionAmount])
-
-	const switchConnectionAmountEl = (connectionAmountEl: number): string => {
-		switch (connectionAmountEl) {
-			case 0:
-				return 'Без пересадок'
-			case 1:
-				return `${connectionAmountEl} пересадка`
-			default:
-				return `${connectionAmountEl} пересадки`
-		}
-	}
 
 	return (
 		<section className={styles.search_air_tickets}>
 			<div>
-				<div>
-					<h3>Количество пересадок</h3>
-					{connectionAmount
-						.sort((a, b) => a - b)
-						.map(connectionAmountEl => (
-							<Checkbox
-								key={connectionAmountEl}
-								active={
-									activeCheckbox.has(`${connectionAmountEl}`) ? true : false
-								}
-								value={connectionAmountEl}
-								onChange={e => handleCheckboxClick(e)}
-							>
-								{switchConnectionAmountEl(connectionAmountEl)}
-							</Checkbox>
-						))}
-				</div>
-				<div>
-					<h3>Компании</h3>
-					{companies.map(company => (
-						<RadioButton
-							key={company}
-							name='company'
-							active={activeRadioBtn}
-							onChange={() => handleRadioBtnClick(company.toLowerCase())}
-						>
-							{company}
-						</RadioButton>
-					))}
-				</div>
+				{searchAirTicketsSortItems.map(searchAirTicketsSortItem => (
+					<SearchAirTicketsSortPanel
+						key={searchAirTicketsSortItem.title}
+						title={searchAirTicketsSortItem.title}
+						content={
+							searchAirTicketsSortItem.variant === 'checkbox'
+								? searchAirTicketsProps.connectionAmount
+								: searchAirTicketsProps.companies
+						}
+						variant={searchAirTicketsSortItem.variant}
+						active={
+							searchAirTicketsSortItem.variant === 'checkbox'
+								? searchAirTicketsProps.activeCheckbox
+								: searchAirTicketsProps.activeRadioBtn
+						}
+						handle={
+							searchAirTicketsSortItem.variant === 'checkbox'
+								? searchAirTicketsProps.handleCheckboxClick
+								: searchAirTicketsProps.handleRadioBtnClick
+						}
+					/>
+				))}
 			</div>
 
 			<div>
@@ -202,48 +70,19 @@ const SearchAirTickets: FunctionComponent<{ [x: string]: ITicketsData[] }> = ({
 						<Button
 							key={index}
 							variant={btnElement.variant}
-							active={activeBtn}
-							onClick={() => handleSortClick(btnElement.variant)}
+							active={searchAirTicketsProps.activeBtn}
+							onClick={() =>
+								searchAirTicketsProps.handleSortClick(btnElement.variant)
+							}
 						>
 							{btnElement.children}
 						</Button>
 					))}
 				</div>
 
-				<div>
-					{ticketsData.map(ticket => (
-						<div key={ticket.id}>
-							<div>
-								<h2>{currency(ticket.price)}</h2>
-								<img src={ticket.logo} alt='Победа' />
-							</div>
-							<div>
-								<div>
-									<div>{`${ticket.from} - ${ticket.to}`}</div>
-									<div>{`${ticket.time.startTime} - ${ticket.time.endTime}`}</div>
-								</div>
-								<div>
-									<div>В пути</div>
-									<div>
-										{`${ticket.duration.hour} ч ${ticket.duration.minute} мин`}
-									</div>
-								</div>
-								<div>
-									<div>Пересадки</div>
-									<div>
-										{ticket.connectionAmount && ticket.connectionAmount === 1
-											? '1 пересадка'
-											: ticket.connectionAmount && ticket.connectionAmount !== 1
-											? `${ticket.connectionAmount} пересадки`
-											: 'Без пересадок'}
-									</div>
-								</div>
-							</div>
-						</div>
-					))}
-
-					<Button variant='load-more'>Загрузить еще билеты</Button>
-				</div>
+				<SearchAirTicketsWrapper
+					ticketsData={searchAirTicketsProps.ticketsData}
+				/>
 			</div>
 		</section>
 	)
