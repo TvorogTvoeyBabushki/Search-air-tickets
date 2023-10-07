f3cc2790caf1953ff7634d5ed64b1da2d45b74c8
foxta1ll <snake3773@mail.ru>
2023-10-07 17:33:00 +0300

    feat: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/Search-air-tickets/',
 	plugins: [react(), tsconfigPaths()],
 	resolve: {
 		alias: {
